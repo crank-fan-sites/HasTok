26c96a2d86b044277561139c3ed007d47a337dc4
Saadh S <atextbooksituation@gmail.com>
2024-09-20 14:04:34 -0400

    Adding analytics
    Did a force push which has messed Vercel deployments up

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 interface PageHeaderProps {
   children: string;
@@ -18,6 +19,7 @@ const PageHeader: React.FC<PageHeaderProps> = ({ children }) => {
           {children}
         </h1>
       </div>
+      <Analytics />
     </div>
   );
 };
