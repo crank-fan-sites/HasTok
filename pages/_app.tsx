b122752571ef643f78a7c5ab9806b1de4be9f7ff
Saadh S <atextbooksituation@gmail.com>
2024-10-01 01:56:20 -0400

    Integrate Plausible analytics with Next.js app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
+import PlausibleProvider from "next-plausible";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider domain="hasanabitv.com" trackOutboundLinks={true}>
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 }
